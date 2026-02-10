767dd433ae22662904bc7be0a9e588ace26f49d9
Noufal <noufalkakdlr@gmail.com>
2026-02-10 13:56:07 +0530

    configure tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,22 @@
 import { Text, View } from "react-native";
+import { useFonts, Geist_400Regular, Geist_500Medium, Geist_700Bold, Geist_900Black } from '@expo-google-fonts/geist'
 
 export default function Page() {
+
+  let [fontsLoaded] = useFonts({
+    Geist_400Regular,
+    Geist_500Medium,
+    Geist_700Bold,
+    Geist_900Black,
+  })
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View className="flex-1 bg-black">
-      <View className="flex-1 w-full max-w-2xl mx-auto bg-white">
-        <Text>Hey, I’m Noufal!</Text>
-        <Text>
+    <View className="flex-1 bg-black ">
+      <View className="flex-1 w-full max-w-2xl mx-auto px-6 ">
+        <Text className="text-white text-4xl mb-4" style={{ fontFamily: 'Geist_700Bold' }}>Hey, I’m Noufal!</Text>
+        <Text className="text-white text-base leading-6">
           I’m a Graphic Designer and a Self-taught Engineer. I spend my time
           leading design teams, building backend APIs with Python, and tinkering
           with Arch Linux. I prefer logic over dogmas and clean code over
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Geist_400Regular'], 
+        medium: ['Geist_500Medium'],
+        bold: ['Geist_700Bold'],
+        black: ['Geist_900Black'],
+      }
+    },
   },
   plugins: [],
 }
